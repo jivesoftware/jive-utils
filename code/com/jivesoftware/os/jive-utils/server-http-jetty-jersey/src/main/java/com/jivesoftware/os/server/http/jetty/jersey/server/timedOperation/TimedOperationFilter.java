ec0ea92629900758e92eda1bc8c59b30f038aa69
Jonathan Colt <jonathan.colt@jivesoftware.com>
2013-11-19 18:00:17 -0800

    added apache license to the top of all java files

--- code/com/jivesoftware/os/jive-utils/server-http-jetty-jersey/src/main/java/com/jivesoftware/os/server/http/jetty/jersey/server/timedOperation/TimedOperationFilter.java
+++ code/com/jivesoftware/os/jive-utils/server-http-jetty-jersey/src/main/java/com/jivesoftware/os/server/http/jetty/jersey/server/timedOperation/TimedOperationFilter.java
@@ -1,15 +1,18 @@
-// "Therefore those skilled at the unorthodox
-// are infinite as heaven and earth,
-// inexhaustible as the great rivers.
-// When they come to an end,
-// they begin again,
-// like the days and months;
-// they die and are reborn,
-// like the four seasons."
-//
-// - Sun Tsu,
-// "The Art of War"
-
+/*
+ * Copyright 2013 Jive Software, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.jivesoftware.os.server.http.jetty.jersey.server.timedOperation;
 
 import com.jivesoftware.os.jive.utils.logger.MetricLogger;
