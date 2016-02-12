e0b5fd8f5250fdd3a766b90933dbb453a2b28a0e
Jonathan Colt <jonathan.colt@jivesoftware.com>
2016-02-12 09:37:21 -1000

    made ConcurrentBAHash lazily allocate stripes

---
+++ collections/src/main/java/com/jivesoftware/os/jive/utils/collections/lh/LHMapStateManager.java
@@ -0,0 +1,18 @@
+package com.jivesoftware.os.jive.utils.collections.lh;
+
+/**
+ *
+ * @author jonathan.colt
+ */
+public class LHMapStateManager<V> {
+
+    void allocated(LHMapState<V> state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    void deallocated(LHMapState<V> state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
