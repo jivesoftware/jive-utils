aa0152a8a64f21e38d3a669362375f8f031e5afd
Kevin Karpenske <kevin.karpenske@jivesoftware.com>
2016-05-16 20:05:35 -0500

    better hash code

--- collections/src/main/java/com/jivesoftware/os/jive/utils/collections/bah/BAHasher.java
+++ collections/src/main/java/com/jivesoftware/os/jive/utils/collections/bah/BAHasher.java
@@ -12,11 +12,18 @@ public class BAHasher {
         return bytes == null ? 0 : compute(bytes, offset, length);
     }
 
-    private int compute(byte[] a, int offset, int length) {
-        int result = 1;
+    private int compute(byte[] bytes, int offset, int length) {
+        int hash = 0;
+        long randMult = 0x5_DEEC_E66DL;
+        long randAdd = 0xBL;
+        long randMask = (1L << 48) - 1;
+        long seed = bytes.length;
         for (int i = offset; i < offset + length; i++) {
-            result = 31 * result + a[i];
+            long x = (seed * randMult + randAdd) & randMask;
+
+            seed = x;
+            hash += (bytes[i] + 128) * x;
         }
-        return result;
+        return hash;
     }
 }
